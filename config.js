e705dea82750e45a792cd1dbb55893e535d5fd07
Owloid <lee2363@purdue.edu>
2016-12-03 17:52:00 -0500

    Move config to index

--- config.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyD7Bqmj2uj_BejEHOj2BLi6bWcZpovIs7s",
-    authDomain: "watch-a-number-go-up.firebaseapp.com",
-    databaseURL: "https://watch-a-number-go-up.firebaseio.com",
-    storageBucket: "watch-a-number-go-up.appspot.com",
-    messagingSenderId: "106334388844"
-};
-firebase.initializeApp(config);
