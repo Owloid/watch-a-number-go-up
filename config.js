dedaf52a8e5dc318920f6c6ed8435f0d495784d4
Owloid <lee2363@purdue.edu>
2016-12-03 17:50:57 -0500

    Add config

---
+++ config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD7Bqmj2uj_BejEHOj2BLi6bWcZpovIs7s",
+    authDomain: "watch-a-number-go-up.firebaseapp.com",
+    databaseURL: "https://watch-a-number-go-up.firebaseio.com",
+    storageBucket: "watch-a-number-go-up.appspot.com",
+    messagingSenderId: "106334388844"
+};
+firebase.initializeApp(config);
